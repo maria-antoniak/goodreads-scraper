199c6458a41af8022438bd706f2083c24a1de951
mah343 <antoniakmaria@gmail.com>
2020-01-12 13:16:57 -0500

    uncommenting test script

--- test_scripts.sh
+++ test_scripts.sh
@@ -1,3 +1,3 @@
-# python get_books.py --book_ids_path test_book_ids.txt --output_directory_path test-output/test-books
+python get_books.py --book_ids_path test_book_ids.txt --output_directory_path test-output/test-books
 
 python get_reviews.py --book_ids_path test_book_ids.txt --output_directory_path test-output/test-reviews --sort_order 0
