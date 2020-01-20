466d68d4029e68c25bb14a8ef9f1ec745b70bfa8
mah343 <antoniakmaria@gmail.com>
2020-01-20 16:01:43 -0500

    test scripts

--- test_scripts.sh
+++ test_scripts.sh
@@ -4,4 +4,4 @@ mkdir -p test-output/test-reviews
 
 python get_books.py --book_ids_path test_book_ids.txt --output_directory_path test-output/test-books
 
-python get_reviews.py --book_ids_path test_book_ids.txt --output_directory_path test-output/test-reviews --sort_order 0
+python get_reviews.py --book_ids_path test_book_ids.txt --output_directory_path test-output/test-reviews --sort_order 0 --browser firefox
