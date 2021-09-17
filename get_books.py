74fc1c885c03072b0c71824e86b540bad36d056a
studs <yabai>
2021-09-17 15:30:00 +0200

    refactor: reformat with black lint

--- get_books.py
+++ get_books.py
@@ -11,24 +11,26 @@ import bs4
 import pandas as pd
 
 
-from config import (config_book_id_title,
-                    config_book_id,
-                    config_book_title,
-                    config_book_series,
-                    config_book_series_uri,
-                    config_isbn,
-                    config_isbn13,
-                    config_year_first_published,
-                    config_author,
-                    config_num_pages,
-                    config_genres,
-                    config_shelves,
-                    config_primary_genre,
-                    config_lists,
-                    config_num_ratings,
-                    config_num_reviews,
-                    config_average_rating,
-                    config_rating_distribution)
+from config import (
+    config_book_id_title,
+    config_book_id,
+    config_book_title,
+    config_book_series,
+    config_book_series_uri,
+    config_isbn,
+    config_isbn13,
+    config_year_first_published,
+    config_author,
+    config_num_pages,
+    config_genres,
+    config_shelves,
+    config_primary_genre,
+    config_lists,
+    config_num_ratings,
+    config_num_reviews,
+    config_average_rating,
+    config_rating_distribution,
+)
 
 
 def get_all_lists(soup):
@@ -36,33 +38,39 @@ def get_all_lists(soup):
     lists = []
     list_count_dict = {}
 
-    if soup.find('a', text='More lists with this book...'):
+    if soup.find("a", text="More lists with this book..."):
 
-        lists_url = soup.find('a', text='More lists with this book...')['href']
+        lists_url = soup.find("a", text="More lists with this book...")["href"]
 
-        source = urlopen('https://www.goodreads.com' + lists_url)
-        soup = bs4.BeautifulSoup(source, 'lxml')
-        lists += [' '.join(node.text.strip().split()) for node in soup.find_all('div', {'class': 'cell'})]
+        source = urlopen("https://www.goodreads.com" + lists_url)
+        soup = bs4.BeautifulSoup(source, "lxml")
+        lists += [
+            " ".join(node.text.strip().split())
+            for node in soup.find_all("div", {"class": "cell"})
+        ]
 
         i = 0
-        while soup.find('a', {'class': 'next_page'}) and i <= 10:
+        while soup.find("a", {"class": "next_page"}) and i <= 10:
 
             time.sleep(2)
-            next_url = 'https://www.goodreads.com' + soup.find('a', {'class': 'next_page'})['href']
+            next_url = (
+                "https://www.goodreads.com"
+                + soup.find("a", {"class": "next_page"})["href"]
+            )
             source = urlopen(next_url)
-            soup = bs4.BeautifulSoup(source, 'lxml')
+            soup = bs4.BeautifulSoup(source, "lxml")
 
-            lists += [node.text for node in soup.find_all('div', {'class': 'cell'})]
+            lists += [node.text for node in soup.find_all("div", {"class": "cell"})]
             i += 1
 
         # Format lists text.
         for _list in lists:
             # _list_name = ' '.join(_list.split()[:-8])
-            # _list_rank = int(_list.split()[-8][:-2]) 
+            # _list_rank = int(_list.split()[-8][:-2])
             # _num_books_on_list = int(_list.split()[-5].replace(',', ''))
             # list_count_dict[_list_name] = _list_rank / float(_num_books_on_list)     # TODO: switch this back to raw counts
             _list_name = _list.split()[:-2][0]
-            _list_count = int(_list.split()[-2].replace(',', ''))
+            _list_count = int(_list.split()[-2].replace(",", ""))
             list_count_dict[_list_name] = _list_count
 
     return list_count_dict
@@ -71,20 +79,23 @@ def get_all_lists(soup):
 def get_shelves(soup):
 
     shelf_count_dict = {}
-    
-    if soup.find('a', text='See top shelves…'):
+
+    if soup.find("a", text="See top shelves…"):
 
         # Find shelves text.
-        shelves_url = soup.find('a', text='See top shelves…')['href']
-        source = urlopen('https://www.goodreads.com' + shelves_url)
-        soup = bs4.BeautifulSoup(source, 'lxml')
-        shelves = [' '.join(node.text.strip().split()) for node in soup.find_all('div', {'class': 'shelfStat'})]
-        
+        shelves_url = soup.find("a", text="See top shelves…")["href"]
+        source = urlopen("https://www.goodreads.com" + shelves_url)
+        soup = bs4.BeautifulSoup(source, "lxml")
+        shelves = [
+            " ".join(node.text.strip().split())
+            for node in soup.find_all("div", {"class": "shelfStat"})
+        ]
+
         # Format shelves text.
         shelf_count_dict = {}
         for _shelf in shelves:
             _shelf_name = _shelf.split()[:-2][0]
-            _shelf_count = int(_shelf.split()[-2].replace(',', ''))
+            _shelf_count = int(_shelf.split()[-2].replace(",", ""))
             shelf_count_dict[_shelf_name] = _shelf_count
 
     return shelf_count_dict
@@ -92,9 +103,11 @@ def get_shelves(soup):
 
 def get_genres(soup):
     genres = []
-    for node in soup.find_all('div', {'class': 'left'}):
-        current_genres = node.find_all('a', {'class': 'actionLinkLite bookPageGenreLink'})
-        current_genre = ' > '.join([g.text for g in current_genres])
+    for node in soup.find_all("div", {"class": "left"}):
+        current_genres = node.find_all(
+            "a", {"class": "actionLinkLite bookPageGenreLink"}
+        )
+        current_genre = " > ".join([g.text for g in current_genres])
         if current_genre.strip():
             genres.append(current_genre)
     return genres
@@ -103,7 +116,7 @@ def get_genres(soup):
 def get_series_name(soup):
     series = soup.find(id="bookSeries").find("a")
     if series:
-        series_name = re.search(r'\((.*?)\)', series.text).group(1)
+        series_name = re.search(r"\((.*?)\)", series.text).group(1)
         return series_name
     else:
         return ""
@@ -120,45 +133,49 @@ def get_series_uri(soup):
 
 def get_isbn(soup):
     try:
-        isbn = re.findall(r'nisbn: [0-9]{10}' , str(soup))[0].split()[1]
+        isbn = re.findall(r"nisbn: [0-9]{10}", str(soup))[0].split()[1]
         return isbn
     except:
         return "isbn not found"
 
+
 def get_isbn13(soup):
     try:
-        isbn13 = re.findall(r'nisbn13: [0-9]{13}' , str(soup))[0].split()[1]
+        isbn13 = re.findall(r"nisbn13: [0-9]{13}", str(soup))[0].split()[1]
         return isbn13
     except:
         return "isbn13 not found"
 
 
 def get_rating_distribution(soup):
-    distribution = re.findall(r'renderRatingGraph\([\s]*\[[0-9,\s]+', str(soup))[0]
-    distribution = ' '.join(distribution.split())
-    distribution = [int(c.strip()) for c in distribution.split('[')[1].split(',')]
-    distribution_dict = {'5 Stars': distribution[0],
-                         '4 Stars': distribution[1],
-                         '3 Stars': distribution[2],
-                         '2 Stars': distribution[3],
-                         '1 Star':  distribution[4]}
+    distribution = re.findall(r"renderRatingGraph\([\s]*\[[0-9,\s]+", str(soup))[0]
+    distribution = " ".join(distribution.split())
+    distribution = [int(c.strip()) for c in distribution.split("[")[1].split(",")]
+    distribution_dict = {
+        "5 Stars": distribution[0],
+        "4 Stars": distribution[1],
+        "3 Stars": distribution[2],
+        "2 Stars": distribution[3],
+        "1 Star": distribution[4],
+    }
     return distribution_dict
 
 
 def get_num_pages(soup):
-    if soup.find('span', {'itemprop': 'numberOfPages'}):
-        num_pages = soup.find('span', {'itemprop': 'numberOfPages'}).text.strip()
+    if soup.find("span", {"itemprop": "numberOfPages"}):
+        num_pages = soup.find("span", {"itemprop": "numberOfPages"}).text.strip()
         return int(num_pages.split()[0])
-    return ''
+    return ""
 
 
 def get_year_first_published(soup):
-    year_first_published = soup.find('nobr', attrs={'class':'greyText'})
+    year_first_published = soup.find("nobr", attrs={"class": "greyText"})
     if year_first_published:
         year_first_published = year_first_published.string
-        return re.search('([0-9]{3,4})', year_first_published).group(1)
+        return re.search("([0-9]{3,4})", year_first_published).group(1)
     else:
-        return ''
+        return ""
+
 
 def get_id(bookid):
     pattern = re.compile("([^.-]+)")
@@ -166,94 +183,147 @@ def get_id(bookid):
 
 
 def scrape_book(book_id):
-    url = 'https://www.goodreads.com/book/show/' + book_id
+    url = "https://www.goodreads.com/book/show/" + book_id
     source = urlopen(url)
-    soup = bs4.BeautifulSoup(source, 'html.parser')
+    soup = bs4.BeautifulSoup(source, "html.parser")
 
     time.sleep(2)
 
     return {
         "book_id_title": book_id if config_book_id_title is True else None,
         "book_id": get_id(book_id) if config_book_id is True else None,
-        "book_title": ' '.join(soup.find('h1', {'id': 'bookTitle'}).text.split()) if config_book_title is True else None,
+        "book_title": " ".join(soup.find("h1", {"id": "bookTitle"}).text.split())
+        if config_book_title is True
+        else None,
         "book_series": get_series_name(soup) if config_book_series is True else None,
-        "book_series_uri": get_series_uri(soup) if config_book_series_uri is True else None,
+        "book_series_uri": get_series_uri(soup)
+        if config_book_series_uri is True
+        else None,
         "isbn": get_isbn(soup) if config_isbn is True else None,
         "isbn13": get_isbn13(soup) if config_isbn13 is True else None,
-        "year_first_published": get_year_first_published(soup) if config_year_first_published is True else None,
-        "author": ' '.join(soup.find('span', {'itemprop': 'name'}).text.split()) if config_author is True else None,
+        "year_first_published": get_year_first_published(soup)
+        if config_year_first_published is True
+        else None,
+        "author": " ".join(soup.find("span", {"itemprop": "name"}).text.split())
+        if config_author is True
+        else None,
         "num_pages": get_num_pages(soup) if config_num_pages is True else None,
         "genres": get_genres(soup) if config_genres is True else None,
         "primary_genre": "STUB" if config_primary_genre is True else None,
         "shelves": get_shelves(soup) if config_shelves is True else None,
         "lists": get_all_lists(soup) if config_lists is True else None,
-        "num_ratings": soup.find('meta', {'itemprop': 'ratingCount'})[
-            'content'].strip() if config_num_ratings is True else None,
-        "num_reviews": soup.find('meta', {'itemprop': 'reviewCount'})[
-            'content'].strip() if config_num_reviews is True else None,
-        "average_rating": soup.find('span',
-                                    {'itemprop': 'ratingValue'}).text.strip() if config_average_rating is True else None,
-        "rating_distribution": get_rating_distribution(soup) if config_rating_distribution is True else None,
-
+        "num_ratings": soup.find("meta", {"itemprop": "ratingCount"})["content"].strip()
+        if config_num_ratings is True
+        else None,
+        "num_reviews": soup.find("meta", {"itemprop": "reviewCount"})["content"].strip()
+        if config_num_reviews is True
+        else None,
+        "average_rating": soup.find("span", {"itemprop": "ratingValue"}).text.strip()
+        if config_average_rating is True
+        else None,
+        "rating_distribution": get_rating_distribution(soup)
+        if config_rating_distribution is True
+        else None,
     }
+
 
 def condense_books(books_directory_path):
 
     books = []
 
     for file_name in os.listdir(books_directory_path):
-        if file_name.endswith('.json') and not file_name.startswith('.') and file_name != "all_books.json":
-            _book = json.load(open(books_directory_path + '/' + file_name, 'r')) #, encoding='utf-8', errors='ignore'))
+        if (
+            file_name.endswith(".json")
+            and not file_name.startswith(".")
+            and file_name != "all_books.json"
+        ):
+            _book = json.load(
+                open(books_directory_path + "/" + file_name, "r")
+            )  # , encoding='utf-8', errors='ignore'))
             books.append(_book)
 
     return books
 
-def main():
 
-    print("config")
-    print(config_book_id_title)
+def main():
 
     start_time = datetime.now()
     script_name = os.path.basename(__file__)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--book_ids_path', type=str)
-    parser.add_argument('--output_directory_path', type=str)
-    parser.add_argument('--format', type=str, action="store", default="json",
-                        dest="format", choices=["json", "csv"],
-                        help="set file output format")
+    parser.add_argument("--book_ids_path", type=str)
+    parser.add_argument("--output_directory_path", type=str)
+    parser.add_argument(
+        "--format",
+        type=str,
+        action="store",
+        default="json",
+        dest="format",
+        choices=["json", "csv"],
+        help="set file output format",
+    )
     args = parser.parse_args()
 
-    book_ids              = [line.strip() for line in open(args.book_ids_path, 'r') if line.strip()]
-    books_already_scraped =  [file_name.replace('.json', '') for file_name in os.listdir(args.output_directory_path) if file_name.endswith('.json') and not file_name.startswith('all_books')]
-    books_to_scrape       = [book_id for book_id in book_ids if book_id not in books_already_scraped]
-    condensed_books_path   = args.output_directory_path + '/all_books'
+    book_ids = [line.strip() for line in open(args.book_ids_path, "r") if line.strip()]
+    books_already_scraped = [
+        file_name.replace(".json", "")
+        for file_name in os.listdir(args.output_directory_path)
+        if file_name.endswith(".json") and not file_name.startswith("all_books")
+    ]
+    books_to_scrape = [
+        book_id for book_id in book_ids if book_id not in books_already_scraped
+    ]
+    condensed_books_path = args.output_directory_path + "/all_books"
 
     for i, book_id in enumerate(books_to_scrape):
         try:
-            print(str(datetime.now()) + ' ' + script_name + ': Scraping ' + book_id + '...')
-            print(str(datetime.now()) + ' ' + script_name + ': #' + str(i+1+len(books_already_scraped)) + ' out of ' + str(len(book_ids)) + ' books')
+            print(
+                str(datetime.now())
+                + " "
+                + script_name
+                + ": Scraping "
+                + book_id
+                + "..."
+            )
+            print(
+                str(datetime.now())
+                + " "
+                + script_name
+                + ": #"
+                + str(i + 1 + len(books_already_scraped))
+                + " out of "
+                + str(len(book_ids))
+                + " books"
+            )
 
             book = scrape_book(book_id)
-            json.dump(book, open(args.output_directory_path + '/' + book_id + '.json', 'w'))
+            json.dump(
+                book, open(args.output_directory_path + "/" + book_id + ".json", "w")
+            )
 
-            print('=============================')
+            print("=============================")
 
         except HTTPError as e:
             print(e)
             exit(0)
 
-
     books = condense_books(args.output_directory_path)
-    if args.format == 'json':
-        json.dump(books, open(f"{condensed_books_path}.json", 'w'))
-    elif args.format == 'csv':
-        json.dump(books, open(f"{condensed_books_path}.json", 'w'))
+    if args.format == "json":
+        json.dump(books, open(f"{condensed_books_path}.json", "w"))
+    elif args.format == "csv":
+        json.dump(books, open(f"{condensed_books_path}.json", "w"))
         book_df = pd.read_json(f"{condensed_books_path}.json")
-        book_df.to_csv(f"{condensed_books_path}.csv", index=False, encoding='utf-8')
+        book_df.to_csv(f"{condensed_books_path}.csv", index=False, encoding="utf-8")
 
-    print(str(datetime.now()) + ' ' + script_name + f':\n\n🎉 Success! All book metadata scraped. 🎉\n\nMetadata files have been output to /{args.output_directory_path}\nGoodreads scraping run time = ⏰ ' + str(datetime.now() - start_time) + ' ⏰')
+    print(
+        str(datetime.now())
+        + " "
+        + script_name
+        + f":\n\n🎉 Success! All book metadata scraped. 🎉\n\nMetadata files have been output to /{args.output_directory_path}\nGoodreads scraping run time = ⏰ "
+        + str(datetime.now() - start_time)
+        + " ⏰"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
